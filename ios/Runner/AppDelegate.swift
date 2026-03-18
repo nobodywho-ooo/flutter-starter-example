1eb27ac8c675f1c5d1e6813bf11f41ade53f378f
Pierre Bresson <262128333+pielouNW@users.noreply.github.com>
2026-03-18 10:40:55 +0100

    naive approach for both ios / android

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -12,5 +12,35 @@ import UIKit
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+
+    let engine = engineBridge.pluginRegistry as! FlutterEngine
+    FlutterMethodChannel(name: "copy_asset", binaryMessenger: engine.binaryMessenger)
+      .setMethodCallHandler { call, result in
+        if call.method == "copyAsset" {
+          let args = call.arguments as! [String: String]
+          let assetPath = args["assetPath"]!
+          let destPath = args["destPath"]!
+          let destURL = URL(fileURLWithPath: destPath)
+          if FileManager.default.fileExists(atPath: destPath) {
+            result(nil)
+            return
+          }
+          let appFrameworkURL = Bundle.main.bundleURL
+            .appendingPathComponent("Frameworks")
+            .appendingPathComponent("App.framework")
+          let bundleURL = appFrameworkURL
+            .appendingPathComponent("flutter_assets")
+            .appendingPathComponent(assetPath)
+          do {
+            try FileManager.default.createDirectory(at: destURL.deletingLastPathComponent(), withIntermediateDirectories: true)
+            try FileManager.default.copyItem(at: bundleURL, to: destURL)
+            result(nil)
+          } catch {
+            result(FlutterError(code: "COPY_FAILED", message: error.localizedDescription, details: nil))
+          }
+        } else {
+          result(FlutterMethodNotImplemented)
+        }
+      }
   }
 }
